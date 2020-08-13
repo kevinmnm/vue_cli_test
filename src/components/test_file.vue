c8a1c9ca36def3da588b228b1989307a094b4a1a
Kevin Song <kevinmnm2010@gmail.com>
2020-08-13 13:54:01 -0400

    testing and learning

---
+++ src/components/test_file.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{ test_txt }}</h2>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    data(){
+        return {
+            test_txt: "Test sentence!!"
+        }
+    }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <test-comp></test-comp>
   </div>
 </template>
+
+<script>
+import aa from '@/components/test_file.vue'
+
+
+export default {
+    name: "About",
+    components: {
+        'test-comp': aa
+    }
+}
+</script>
